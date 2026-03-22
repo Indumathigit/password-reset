c2e265069023298c107989db75de3051bec503eb
Indumathi M <indumathimurugan745@gmail.com>
2026-03-22 08:15:14 +0530

    fix brevo api sdk

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,12 +1,13 @@
-const Brevo = require("@getbrevo/brevo");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 const sendEmail = async (to, subject, text) => {
-  var apiInstance = new Brevo.TransactionalEmailsApi();
-
-  var apiKey = apiInstance.authentications["apiKey"];
+  var defaultClient = SibApiV3Sdk.ApiClient.instance;
+  var apiKey = defaultClient.authentications["api-key"];
   apiKey.apiKey = process.env.BREVO_API_KEY;
 
-  var sendSmtpEmail = new Brevo.SendSmtpEmail();
+  var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+  var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
   sendSmtpEmail.to = [{ email: to }];
   sendSmtpEmail.sender = { email: "indumathimurugan745@gmail.com", name: "Password Reset" };
   sendSmtpEmail.subject = subject;
